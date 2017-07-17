3861c107221078e8a8e8598fac770742b31cc11f
坏脾气女生 <597053471@qq.com>
2017-07-17 21:09:14 +0800

    03-添加任务功能完成

--- js/app.js
+++ js/app.js
@@ -15,5 +15,21 @@
 				{ id:4, name: '复习', isCompleted:false},
 			];
 			vm.todoList=todoList;
+
+			//2 添加任务
+			vm.taskName='';
+			vm.add=function(){
+				if(vm.taskName.trim()===''){
+					return;
+				}
+				var id,length=todoList.length;
+				if(length<=0){
+					id=1;
+				}else{
+					id=todoList[length-1]+1;
+				}
+				todoList.push({ id:id, name:vm.taskName, isCompleted:false });
+				vm.taskName='';
+			}
 		}
 })(angular);
