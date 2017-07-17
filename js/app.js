db827f158bd39859b4624c49b2373947172fee1b
坏脾气女生 <597053471@qq.com>
2017-07-17 20:58:52 +0800

    02-展示任务列表完成

--- js/app.js
+++ js/app.js
@@ -6,6 +6,14 @@
 	
 		function TodoController($scope){
 			// Your starting point. Enjoy the ride!
-			
+			//1 展示列表
+			var vm =$scope;
+			var todoList=[
+				{ id:1, name: '预习', isCompleted:true},
+				{ id:2, name: '学习Angular', isCompleted:false},
+				{ id:3, name: '代码练习', isCompleted:false},
+				{ id:4, name: '复习', isCompleted:false},
+			];
+			vm.todoList=todoList;
 		}
-})(window);
+})(angular);
