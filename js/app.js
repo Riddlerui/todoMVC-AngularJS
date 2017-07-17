5edf98f87d6eda00aa65c8fedff3e275e27bb66f
坏脾气女生 <597053471@qq.com>
2017-07-17 20:47:59 +0800

    01-基本结构搭建完成

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function (angular) {
+	'use strict';
+	angular
+		.module('todoApp',[])
+		.controller('todoController',['$scope', TodoController])
+	
+		function TodoController($scope){
+			// Your starting point. Enjoy the ride!
+			
+		}
+})(window);
