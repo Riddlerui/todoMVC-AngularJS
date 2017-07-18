e2c769cf287938b73f6a0f2cbd2acf3d1be1c4e2
坏脾气女生 <597053471@qq.com>
2017-07-18 20:22:53 +0800

    12-将数据操作移动到服务中(一)

--- js/controller.js
+++ js/controller.js
@@ -1,131 +1,90 @@
-(function(angular){
-    'user strict';
+(function (angular) {
+	'user strict';
 
-    angular
-    .module('todoApp.ctrl',[])
-    .controller('todoController',['$scope','$location','todoSrv', TodoController])
-	
-		function TodoController($scope,$location,todoSrv){
-			// Your starting point. Enjoy the ride!
+	angular
+		.module('todoApp.ctrl', [])
+		.controller('todoController', ['$scope', '$location', 'TodoSrv', TodoController])
+
+	function TodoController($scope, $location, TodoSrv) {
+		// Your starting point. Enjoy the ride!
+
+		var vm = $scope;
+
+		//1 展示列表
+		var todoList = TodoSrv.getData();
+		vm.todoList = todoList;
+
+		//2 添加任务
+		vm.taskName = '';
+		vm.add = function () {
+			if (vm.taskName.trim() === '') {
+				return;
+			}
+
+			TodoSrv.add(vm.taskName);
+
+			vm.taskName = '';
+		};
+
+		// 3.删除一条任务
+		vm.del = TodoSrv.del;
+
+
+		// 4 修改任务
+		vm.editingId = -1;
+		vm.edit = function (id) {
+			vm.editingId = id;
+
+		};
+		vm.editSave = function () {
+			vm.editingId = -1;
+			TodoSrv.save();
+		};
+
+		//5 切换任务选中状态(批量)
+		vm.isCheckedAll = false;
+		vm.checkAll = function () {
+			TodoSrv.checkAll(vm.isCheckedAll);
+		};
+
+		//6 清除已完成的任务
+		//保留未完成的任务
+		vm.delCompleted = TodoSrv.delCompleted;
+
+		//6.1 控制清除任务按钮的展示和隐藏
+		vm.isShow = TodoSrv.isShow;
+
+		// 7 显示未完成任务数
+		vm.getCount = TodoSrv.getCount;
 		
-			var vm =$scope;
-			
-			//1 展示列表
-            var todoList=todoSrv.getData();
-			vm.todoList=todoList;
+		// 8 显示不同状态的任务 以及当前任务高亮处理
+		vm.status = false;
+		// vm.selectedAll=function(){
+		// 	vm.status=undefined;
+		// };
+		// vm.selectedActive=function(){
+		// 	vm.status=false;
+		// };
+		// vm.selectedCompleted=function(){
+		// 	vm.status=true;
+		// };
 
-			//2 添加任务
-			vm.taskName='';
-			vm.add=function(){
-				if(vm.taskName.trim()===''){
-					return;
-				}
-
-                todoSrv.add(vm.taskName);
-
-				vm.taskName='';
+		// 9 根据URL变化显示相应任务
+		vm.location = $location;
+		vm.$watch('location.url()', function (newVal, oldVal) {
+			console.log(newVal);
+			switch (newVal) {
+				case '/active':
+					vm.status = false;
+					break;
+				case '/completed':
+					vm.status = true;
+					break;
+				default:
+					vm.status = undefined;
+					break;
 			}
 
-			// 3.删除一条任务
-			vm.del=function(id){
-				for(var i=0;i<todoList.length;i++){
-					if(todoList[i].id===id){
-						todoList.splice(i,1);
-						break;
-					}
-				}
-			}
-			
-
-			// 4 修改任务
-			vm.editingId=-1;
-			vm.edit=function(id){
-				vm.editingId=id;
-			};
-			vm.editSave=function(){
-				vm.editingId=-1;
-			};
-
-			//5 切换任务选中状态(批量)
-			vm.isCheckedAll=false;
-			vm.checkAll=function(){
-				console.log(1)
-				for(var i=0;i<todoList.length;i++){
-					todoList[i].isCompleted=vm.isCheckedAll;
-				}
-			};
-
-			//6 清除已完成的任务
-			//保留未完成的任务
-			vm.delCompleted=function(){
-				var tempArr=[];
-				for(var i=0; i<todoList.length;i++){
-					if(!todoList[i].isCompleted){
-						tempArr.push(todoList[i]);
-					}
-				}
-
-				//赋值
-				// vm.todoList=todoList=tempArr;
-
-				//清空数组
-				todoList.length=0;
-				[].push.apply(todoList,tempArr);
-			};
-			//6.1 控制清除任务按钮的展示和隐藏
-			vm.isShow = function(){
-				var ret=false;
-				for(var i=0;i<todoList.length;i++){
-					if(todoList[i].isCompleted){
-						ret=true;
-						break;
-					}
-				}
-				// ret=todoList.some(function(v){
-				// 	v.isCompleted==true;
-				// 	break;
-				// })
-				return ret;
-			}
-
-			// 7 显示未完成任务数
-			vm.getCount=function(){
-				var count=0;
-				for(var i=0;i<todoList.length; i++){
-					if(!todoList[i].isCompleted){
-						count++;
-					}
-				}
-				return count;
-			};
-
-			// 8 显示不同状态的任务 以及当前任务高亮处理
-			vm.status=false;
-			// vm.selectedAll=function(){
-			// 	vm.status=undefined;
-			// };
-			// vm.selectedActive=function(){
-			// 	vm.status=false;
-			// };
-			// vm.selectedCompleted=function(){
-			// 	vm.status=true;
-			// };
-
-			// 9 根据URL变化显示相应任务
-			vm.location=$location;
-			vm.$watch('location.url()',function(newVal,oldVal){
-				console.log(newVal);
-				switch(newVal){
-					case '/active':
-						vm.status=false;
-						break;
-					case '/completed':
-						vm.status=true;
-						break;
-					default:
-						vm.status=undefined;
-						break;
-				}
-			});
-		}
+		});
+	}
 })(angular);
--- js/service.js
+++ js/service.js
@@ -1,48 +1,110 @@
-(function(angular){
-    'user strict';
-    //
-    angular
-    .module('todoApp.service',[])
-    .service('TodoSrv',['$window', function ($scope){
-        var todoList=[
-				{ id:1, name: '预习', isCompleted:true},
-				{ id:2, name: '学习Angular', isCompleted:false},
-				{ id:3, name: '代码练习', isCompleted:false},
-				{ id:4, name: '复习', isCompleted:false},
-			];
+(function (angular) {
+	'user strict';
+	//
+	angular
+		.module('todoApp.service', [])
+		.service('TodoSrv', ['$window', function ($window) {
+			// var todoList=[
+			// 		{ id:1, name: '预习', isCompleted:true},
+			// 		{ id:2, name: '学习Angular', isCompleted:false},
+			// 		{ id:3, name: '代码练习', isCompleted:false},
+			// 		{ id:4, name: '复习', isCompleted:false},
+			// ];
 
-        // 从localStorage中获取数据
-        var localStorage=$window.localStorage;
-        var todoList=JSON.parse(localStorage.getItem('todo'))||[];
-        //保存数据
-        this.save=function(){
-            localStorage.setItem('todo',JSON.stringify(todoList));
-        };
-
-        var that=this;    
-        
-        //获取数据的方法
-        this.getData=function(){
-            return todoList;
-        }
-
-        //添加数据
-        this.add=function(taskName){
-			var id,length=todoList.length;
-			if(length<=0){
-				id=1;
-			}else{
-				id=todoList[todoList.length-1].id+1;
-			}
-
-			todoList.push({ id:id, name:taskName, isCompleted:false });
-			
-            that.save();
+			// 从localStorage中获取数据
+			var localStorage = $window.localStorage;
+			var todoList = JSON.parse(localStorage.getItem('todo')) || [];
+			// 保存数据
+			this.save = function () {
+				localStorage.setItem('todo', JSON.stringify(todoList));
 			};
 
+			var that = this;
 
-    }]);
+			//获取数据的方法
+			this.getData = function () {
+				return todoList;
+			}
 
-    
-   
+			//添加数据
+			this.add = function (taskName) {
+				var id, length = todoList.length;
+				if (length === 0) {
+					id = 1;
+				} else {
+					id = todoList[todoList.length - 1].id + 1;
+				}
+
+				todoList.push({
+					id: id,
+					name: taskName,
+					isCompleted: false
+				});
+
+				that.save();
+			};
+
+			//删除一条任务
+			this.del = function (id) {
+				for (var i = 0; i < todoList.length; i++) {
+					if (todoList[i].id === id) {
+						todoList.splice(i, 1);
+						break;
+					}
+				}
+				that.save();
+			};
+
+			//全选
+			this.checkAll = function (isCheckedAll) {
+				for (var i = 0; i < todoList.length; i++) {
+					todoList[i].isCompleted = isCheckedAll;
+				}
+				that.save();
+			};
+
+			//清除已完成的任务
+			this.delCompleted = function () {
+				var tempArr = [];
+				for (var i = 0; i < todoList.length; i++) {
+					if (!todoList[i].isCompleted) {
+						tempArr.push(todoList[i]);
+					}
+				}
+
+				//赋值
+				// vm.todoList=todoList=tempArr;
+
+				//清空数组
+				todoList.length = 0;
+				[].push.apply(todoList, tempArr);
+				that.save();
+			};
+			//清除按钮的显示与隐藏
+			this.isShow = function () {
+				var ret = false;
+				for (var i = 0; i < todoList.length; i++) {
+					if (todoList[i].isCompleted) {
+						ret = true;
+						break;
+					}
+				}
+				return ret;
+			};
+
+			//显示未完成任务数
+			this.getCount = function () {
+				var count = 0;
+				for (var i = 0; i < todoList.length; i++) {
+					if (!todoList[i].isCompleted) {
+						count++;
+					}
+				}
+				return count;
+			};
+
+		}]);
+
+
+
 })(angular);
